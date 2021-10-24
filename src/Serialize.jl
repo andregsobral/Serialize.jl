e40070ae74661228c83765db95110652950b9cf9
GibOps <andre.g.sobral@gmail.com>
2021-10-24 23:25:16 +0100

    Update global type mapping setters

--- src/Serialize.jl
+++ src/Serialize.jl
@@ -7,7 +7,10 @@ using JSON
 using Mongoc
 
 TYPE_MAP = Dict{Symbol, DataType}()
-typemap!(d::Dict{Symbol, DataType}) = TYPE_MAP = d
+function typemap!(d::Dict{Symbol, DataType})
+  global TYPE_MAP = d
+end
+typemap() = TYPE_MAP
 
 include("adapters/MongoDB.jl")
 
